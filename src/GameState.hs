665881dae1796de17f6bb5576813ec947d7f0379
Voloshin Bogdan <ya.bovi2012@yandex.ru>
2015-12-27 10:19:54 +0300

    Исправлена ошибка в makeMove, добавлено ещё несколько функций обработки игровых состояний

--- src/GameState.hs
+++ src/GameState.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, ConstraintKinds, AllowAmbiguousTypes, RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts, ConstraintKinds, RecordWildCards #-}
 
 module GameState where
 
@@ -11,12 +11,16 @@ import Data.Array
 import Data.Array.ST
 import Data.Array.MArray
 import Prelude hiding (Either(Left),Either(Right))
+import Control.Monad.Trans.Identity
+import Control.Monad.Identity
+import Data.Maybe
 
 data Cell = Empty | Piece Int deriving (Show)
 data Move = Left | Right | Up | Down deriving (Enum, Bounded, Show)
 
 type IndType = (Int,Int)
 type GameState s = STArray s IndType Cell
+-- ConstraintKinds
 type GameStateMonad s m =  MArray (STArray s) Cell m
 data GameData s = GameData { state :: GameState s, emptyInd :: IndType }
 data FieldData = FieldData { size :: Int }
@@ -27,27 +31,56 @@ allMoves = [minBound :: Move ..]
 finishData :: ST s (GameData s)
 finishData = undefined
 
+-- RecordWildCards
+-- FlexibleContexts 
 makeMove :: (MS.MonadState (GameData s) m, GameStateMonad s m, MonadReader FieldData m) => Move -> m ()
 makeMove move = do
   GameData{..} <- MS.get
   FieldData{..} <- ask
-  let next = indMove size emptyInd move
-  swapElems emptyInd next state
+  case (indMove size emptyInd move) of
+    Just next -> do
+      swapElems emptyInd next state
+      let emptyInd = next in MS.put GameData{..}
+    Nothing -> return ()
+
+indMove :: Int -> IndType -> Move -> Maybe IndType
+indMove size empty move = let (x,y) = indMove' empty move in
+  if x < 0 || x > size-1
+  || y < 0 || y > size-1 then
+    Nothing
+  else
+    Just (x,y)
  where
-
-  indMove :: Int -> IndType -> Move -> IndType
-  indMove size empty move = let (x,y) = indMove' empty move in
-    if x < 0 || x > size-1
-    || y < 0 || y > size-1 then
-      error "Incorrect move"
-    else
-      (x,y)
-
   indMove' :: IndType -> Move -> IndType
   indMove' (x,y) Left = (x-1,y)
   indMove' (x,y) Right = (x+1,y)
-  indMove' (x,y) Up = (x,y+1)
-  indMove' (x,y) Down = (x,y-1)
+  indMove' (x,y) Up = (x,y-1)
+  indMove' (x,y) Down = (x,y+1)
+
+moveFromIndex :: (MS.MonadState (GameData s) m, GameStateMonad s m, MonadReader FieldData m) => IndType -> m (Maybe Move)
+moveFromIndex (x,y) = do
+  GameData{..} <- MS.get
+  FieldData{..} <- ask
+  let (xe,ye) = emptyInd
+  return $ vectorToMove (x - xe, y - ye)
+ where
+  vectorToMove (-1,0) = Just Left
+  vectorToMove (1,0) = Just Right
+  vectorToMove (0,-1) = Just Up
+  vectorToMove (0,1) = Just Down
+  vectorToMove _ = Nothing
+
+isMoveCorrect :: (MS.MonadState (GameData s) m, GameStateMonad s m, MonadReader FieldData m) => Move -> m Bool
+isMoveCorrect move = do
+  GameData{..} <- MS.get
+  FieldData{..} <- ask
+  return $ isJust $ indMove size emptyInd move
+
+nextMoves :: (MS.MonadState (GameData s) m, GameStateMonad s m, MonadReader FieldData m) => m [Move]
+nextMoves = do
+  GameData{..} <- MS.get
+  FieldData{..} <- ask
+  return $ filter (isJust . indMove size emptyInd) allMoves
 
 swapElems :: (MArray (STArray s) e m, Ix i) => i -> i -> STArray s i e -> m ()
 swapElems i j arr = do
