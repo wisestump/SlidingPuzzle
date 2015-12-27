452252c22800a782f81378f491fb7f0a1ba83f2c
Voloshin Bogdan <ya.bovi2012@yandex.ru>
2015-12-27 23:11:00 +0300

    Исправлена GameData в модуле не монадических вычислений(не передавался размер)

--- src/SimpleGameState.hs
+++ src/SimpleGameState.hs
@@ -5,7 +5,7 @@ import System.Random
 
 data Cell = Empty | Piece Int deriving (Show)
 data Move = Left | Right | Up | Down deriving (Enum, Bounded, Show, Read)
-data GameData = GameData { state :: GameState, emptyInd :: IndType }
+data GameData = GameData { state :: GameState, emptyInd :: IndType, size :: Int }
 
 type IndType = (Int,Int)
 type GameState = Map.Map IndType Cell
